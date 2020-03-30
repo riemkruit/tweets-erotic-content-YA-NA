2dce70410b6e4ccd31e85ddaf07d88e702b04e44
riemkruit <r.k.ruitenbeek@student.rug.nl>
2020-03-30 19:11:24 +0200

    Add commands.sh file

---
+++ commands.sh
@@ -0,0 +1,17 @@
+# REMINDER: this is not a shell script that can be executed like this. Read the README for more information on how to use these lines of code.
+
+# CHANGE_MONTH:
+cd /net/corpora/twitter2/Tweets/2019/[MONTH]
+# Make sure to replace [MONTH] with the correct number as specified in the README.
+
+# YA+BOEK-SEKS:
+zless *.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | grep -va '^RT' | grep -i 'young.adult' | grep -i 'boek' | grep -vi 'seks' | wc -l
+
+# YA+BOEK+SEKS:
+zless *.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | grep -va '^RT' | grep -i 'young.adult' | grep -i 'boek' | grep -i 'seks' | wc -l
+
+# NA+BOEK-SEKS:
+zless *.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | grep -va '^RT' | grep -i 'new.adult' | grep -i 'boek' | grep -vi 'seks' | wc -l
+
+# NA+BOEK+SEKS:
+zless *.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | grep -va '^RT' | grep -i 'new.adult' | grep -i 'boek' | grep -i 'seks' | wc -l
